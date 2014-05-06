703ef2959fe19233b24b21ffa6f5869cceb8f075
FrDH <info@frebsite.nl>
2014-05-06 15:07:25 +0200

    widescreen css bugfix

--- src/js/jquery.mmenu.js
+++ src/js/jquery.mmenu.js
@@ -1,5 +1,5 @@
 /*	
- * jQuery mmenu v4.2.5
+ * jQuery mmenu v4.2.6
  * @requires jQuery 1.7.0 or later
  *
  * mmenu.frebsite.nl
@@ -16,7 +16,7 @@
 (function( $ ) {
 
 	var _PLUGIN_	= 'mmenu',
-		_VERSION_	= '4.2.5';
+		_VERSION_	= '4.2.6';
 
 
 	//	Plugin already excists
