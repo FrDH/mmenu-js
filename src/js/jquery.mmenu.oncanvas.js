65e3a6b43a2e8c77abaeae7c86ccc53bc57cd1ee
FrDH <info@frebsite.nl>
2014-06-20 20:17:07 +0200

    new version

--- src/js/jquery.mmenu.oncanvas.js
+++ src/js/jquery.mmenu.oncanvas.js
@@ -1,5 +1,5 @@
 /*	
- * jQuery mmenu v4.3.1
+ * jQuery mmenu v4.3.2
  * @requires jQuery 1.7.0 or later
  *
  * mmenu.frebsite.nl
@@ -16,7 +16,7 @@
 (function( $ ) {
 
 	var _PLUGIN_	= 'mmenu',
-		_VERSION_	= '4.3.1';
+		_VERSION_	= '4.3.2';
 
 
 	//	Plugin already excists
