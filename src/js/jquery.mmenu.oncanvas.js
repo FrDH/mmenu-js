7d8b00a21bdd85aaa0e4cc27cff06c78d5bb6d64
FrDH <info@frebsite.nl>
2014-07-01 19:43:56 +0200

    improved off canvas CSS

--- src/js/jquery.mmenu.oncanvas.js
+++ src/js/jquery.mmenu.oncanvas.js
@@ -1,5 +1,5 @@
 /*	
- * jQuery mmenu v4.3.2
+ * jQuery mmenu v4.3.3
  * @requires jQuery 1.7.0 or later
  *
  * mmenu.frebsite.nl
@@ -15,7 +15,7 @@
 (function( $ ) {
 
 	var _PLUGIN_	= 'mmenu',
-		_VERSION_	= '4.3.2';
+		_VERSION_	= '4.3.3';
 
 
 	//	Plugin already excists
