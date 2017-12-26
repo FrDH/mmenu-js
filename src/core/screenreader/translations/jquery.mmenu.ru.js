7cbf260bb264c10f56a0dd2a19328d7b0f0a0c3d
smartcorestudio <studio@smartcore.ru>
2017-12-26 09:22:00 +0300

    Create jquery.mmenu.ru.js

---
+++ src/core/screenreader/translations/jquery.mmenu.ru.js
@@ -0,0 +1,12 @@
+(function( $ ) {
+
+	var _PLUGIN_ = 'mmenu';
+
+	$[ _PLUGIN_ ].i18n({
+		'Close menu'		: 'Закрыть меню',
+		'Close submenu'		: 'Закрыть подменю',
+		'Open submenu' 		: 'Открыть подменю',
+		'Toggle submenu' 	: 'Переключить подменю'
+	});
+
+})( jQuery );
