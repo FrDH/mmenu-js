a9800863be5e34f2919c7ba948984e7a9cc798d4
smartcorestudio <studio@smartcore.ru>
2017-12-26 09:23:36 +0300

    Create jquery.mmenu.ru.js

---
+++ src/addons/searchfield/translations/jquery.mmenu.ru.js
@@ -0,0 +1,11 @@
+(function( $ ) {
+
+	const _PLUGIN_ = 'mmenu';
+
+	$[ _PLUGIN_ ].i18n({
+		'Search'			: 'Найти',
+		'No results found.'	: 'Ничего не найдено.',
+		'Search results'	: 'Результаты поиска'
+	});
+
+})( jQuery );
