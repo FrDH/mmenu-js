1661a878a447d7452607035fab316017bb741139
Fred Heusschen <fredheusschen@hotmail.com>
2019-02-03 00:05:38 +0100

    added persian-fa translation

---
+++ src/core/oncanvas/translations/jquery.mmenu.fa.ts
@@ -0,0 +1,10 @@
+(function( $ ) {
+
+	const _PLUGIN_ 	= 'mmenu';
+	const _LANG_ 	= 'fa';
+
+	$[ _PLUGIN_ ].i18n({
+		'Menu': 'منو'
+	}, _LANG_ );
+
+})( jQuery );
---
+++ src/core/screenreader/translations/jquery.mmenu.fa.ts
@@ -0,0 +1,13 @@
+(function( $ ) {
+
+	const _PLUGIN_ 	= 'mmenu';
+	const _LANG_ 	= 'fa';
+
+	$[ _PLUGIN_ ].i18n({
+		'Close menu'		: 'بستن منو',
+		'Close submenu'		: 'بستن زیرمنو',
+		'Open submenu' 		: 'بازکردن زیرمنو',
+		'Toggle submenu' 	: 'سوییچ زیرمنو'
+	}, _LANG_ );
+
+})( jQuery );
