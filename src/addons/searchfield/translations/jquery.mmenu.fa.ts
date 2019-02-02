669a06cf01967994981ab405e198e8e04281199b
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-02 16:22:51 +0330

    add persian translation

---
+++ src/addons/searchfield/translations/jquery.mmenu.fa.ts
@@ -0,0 +1,12 @@
+(function( $ ) {
+
+	const _PLUGIN_ 	= 'mmenu';
+	const _LANG_ 	= 'fa';
+
+	$[ _PLUGIN_ ].i18n({
+		'Search'			: 'جستجو',
+		'No results found.'	: 'نتیجه‌ای یافت نشد.',
+		'cancel'			: 'انصراف'
+	}, _LANG_ );
+
+})( jQuery );
