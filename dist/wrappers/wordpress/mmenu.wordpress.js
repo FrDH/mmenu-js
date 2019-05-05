d7c8f893a087c11daa31e4244b366656791c37f0
Fred Heusschen <fredheusschen@hotmail.com>
2019-05-05 10:36:03 +0200

    wordpress wrapper bugfix

--- dist/wrappers/wordpress/mmenu.wordpress.js
+++ dist/wrappers/wordpress/mmenu.wordpress.js
@@ -1,7 +1,8 @@
 export default function () {
     this.conf.classNames.selected = 'current-menu-item';
     var wpadminbar = document.getElementById('wpadminbar');
-    wpadminbar.style.position = 'fixed';
-    wpadminbar.classList.add('mm-slideout');
+    if (wpadminbar) {
+        wpadminbar.style.position = 'fixed';
+        wpadminbar.classList.add('mm-slideout');
+    }
 }
-;
--- src/wrappers/wordpress/mmenu.wordpress.ts
+++ src/wrappers/wordpress/mmenu.wordpress.ts
@@ -1,11 +1,11 @@
 import Mmenu from '../../core/oncanvas/mmenu.oncanvas';
 
-export default function(
-	this : Mmenu
-) {
-	this.conf.classNames.selected = 'current-menu-item';
+export default function(this: Mmenu) {
+    this.conf.classNames.selected = 'current-menu-item';
 
-	var wpadminbar = document.getElementById( 'wpadminbar' );
-	wpadminbar.style.position = 'fixed';
-	wpadminbar.classList.add( 'mm-slideout' );
-};
+    var wpadminbar = document.getElementById('wpadminbar');
+    if (wpadminbar) {
+        wpadminbar.style.position = 'fixed';
+        wpadminbar.classList.add('mm-slideout');
+    }
+}
