84369ccfea88afcbb61c280b8453d052a4d96f2b
Fred Heusschen <fredheusschen@hotmail.com>
2019-06-10 00:01:33 +0200

    scrollbugfix improved

--- dist/core/scrollbugfix/mmenu.scrollbugfix.js
+++ dist/core/scrollbugfix/mmenu.scrollbugfix.js
@@ -3,7 +3,7 @@ import options from './_options';
 import * as DOM from '../_dom';
 import * as support from '../../core/_support';
 import { extendShorthandOptions } from './_options';
-import { extend } from '../../core/_helpers';
+import { extend, touchDirection } from '../../core/_helpers';
 //  Add the options.
 Mmenu.options.scrollBugFix = options;
 export default function () {
@@ -23,54 +23,58 @@ export default function () {
     if (!options.fix) {
         return;
     }
-    //	When opening the menu, scroll to the top of the current opened panel.
-    this.bind('open:start', () => {
-        DOM.children(this.node.pnls, '.mm-panel_opened')[0].scrollTop = 0;
-    });
-    //	Only needs to be done once per page.
-    if (!Mmenu.vars.scrollBugFixed) {
-        let scrolling = false;
-        //	Prevent the body from scrolling.
-        document.addEventListener('touchmove', evnt => {
-            if (this.node.wrpr.matches('.mm-wrapper_opened')) {
-                evnt.preventDefault();
-            }
-        });
-        document.body.addEventListener('touchstart', evnt => {
-            var panel = evnt.target;
-            if (!panel.matches('.mm-panels > .mm-panel')) {
-                return;
-            }
-            if (this.node.wrpr.matches('.mm-wrapper_opened')) {
-                if (!scrolling) {
-                    //	Since we're potentially scrolling the panel in the onScroll event,
-                    //	this little hack prevents an infinite loop.
-                    scrolling = true;
-                    if (panel.scrollTop === 0) {
-                        panel.scrollTop = 1;
-                    }
-                    else if (panel.scrollHeight ===
-                        panel.scrollTop + panel.offsetHeight) {
-                        panel.scrollTop -= 1;
-                    }
-                    //	End of infinite loop preventing hack.
-                    scrolling = false;
-                }
-            }
-        });
-        document.body.addEventListener('touchmove', evnt => {
-            var panel = evnt.target;
-            if (!panel.matches('.mm-panels > .mm-panel')) {
-                return;
-            }
-            if (this.node.wrpr.matches('.mm-wrapper_opened')) {
-                if (panel.scrollHeight > panel.clientHeight) {
-                    evnt.stopPropagation();
-                }
-            }
-        });
+    var touchDir = touchDirection(this.node.menu);
+    /**
+     * Prevent an event from doing its default and stop its propagation.
+     * @param {ScrollBehavior} evnt The event to stop.
+     */
+    function stop(evnt) {
+        evnt.preventDefault();
+        evnt.stopPropagation();
     }
-    Mmenu.vars.scrollBugFixed = true;
+    //  Prevent the page from scrolling when scrolling in the menu.
+    this.node.menu.addEventListener('scroll', stop, {
+        //  Make sure to tell the browser the event will be prevented.
+        passive: false
+    });
+    //  Prevent the page from scrolling when dragging in the menu.
+    this.node.menu.addEventListener('touchmove', evnt => {
+        var panel = evnt.target.closest('.mm-panel');
+        if (panel) {
+            //  When dragging a non-scrollable panel,
+            //      we can simple preventDefault and stopPropagation.
+            if (panel.scrollHeight === panel.offsetHeight) {
+                stop(evnt);
+                //  When dragging a scrollable panel,
+                //      that is fully scrolled up (or down).
+                //      It will not trigger the scroll event when dragging down (or up) (because you can't scroll up (or down)),
+                //      so we need to match the dragging direction with the scroll position before preventDefault and stopPropagation,
+                //      otherwise the panel would not scroll at all in any direction.
+            }
+            else if (
+            //  When scrolled up and dragging down
+            (panel.scrollTop == 0 && touchDir.get() == 'down') ||
+                //  When scrolled down and dragging up
+                (panel.scrollHeight ==
+                    panel.scrollTop + panel.offsetHeight &&
+                    touchDir.get() == 'up')) {
+                stop(evnt);
+            }
+            //  When dragging anything other than a panel.
+        }
+        else {
+            stop(evnt);
+        }
+    }, {
+        //  Make sure to tell the browser the event can be prevented.
+        passive: false
+    });
+    //  Some small additional improvements
+    //	Scroll the current opened panel to the top when opening the menu.
+    this.bind('open:start', () => {
+        var panel = DOM.children(this.node.pnls, '.mm-panel_opened')[0];
+        panel.scrollTop = 0;
+    });
     //	Fix issue after device rotation change.
     window.addEventListener('orientationchange', evnt => {
         var panel = DOM.children(this.node.pnls, '.mm-panel_opened')[0];
